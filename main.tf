9650e420f97c3dc7e8ec5e8d3e7a77916118c821
shabana syed <althafhussainj@gmail.com>
2024-08-14 19:08:48 +0530

    versioning enabled

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "my-new-bucket" {
   bucket = "my-new-s3-bucket12211"
+  acl = "private"
 
+  versioning {
+    enabled = true
+  }
   tags = {
     Name = "my-new-bucket"
     Environment = "Dev"
